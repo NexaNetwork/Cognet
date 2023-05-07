45fcd85809b3cea1dca25d10fc2755b467a6f56d
FlyNightSky <streamafe@fbi.ac>
2023-05-07 18:33:55 -0400

    d

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary: '#F34939',
+				darkGray: '#5C5B5B',
+				secondary:{
+					100:'#EA8780',
+					200: '#A33327',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
