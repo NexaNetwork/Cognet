ddb932f8d2774911f53f7fac065d8e414bb66416
FlyNightSky <118761018+FlyNightSky@users.noreply.github.com>
2023-05-08 21:27:29 -0400

    2 files modified

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors:{
-				primary: '#F34939',
-				darkGray: '#5C5B5B',
-				secondary:{
-					100:'#EA8780',
-					200: '#A33327',
-				}
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F34939',
+        darkGray: '#5C5B5B',
+        secondary: {
+          100: '#EA8780',
+          200: '#A33327',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
